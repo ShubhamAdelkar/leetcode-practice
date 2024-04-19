ed2211f205d662e7629b3051a28751044a1ade1f
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-04-19 10:18:00 +0530

    intersection easy with sets

---
+++ arrays/intersection.js
@@ -0,0 +1,11 @@
+function intersection(nums1, nums2) {
+  const newSet = new Set();
+  for (let i = 0; i < nums1.length; i++) {
+    if (nums2.includes(nums1[i])) {
+      newSet.add(nums1[i]);
+    }
+  }
+  return Array.from(newSet);
+}
+
+console.log(intersection([1, 2, 2, 1], [2, 2]));
