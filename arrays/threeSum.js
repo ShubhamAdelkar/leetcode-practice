b337780d8da2250b2d23d1f93e2a01742e4e8c96
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-06-22 21:42:55 +0530

    update code

--- arrays/threeSum.js
+++ arrays/threeSum.js
@@ -1,6 +1,6 @@
 function threeSum(numbers) {
   numbers.sort((a, b) => a - b);
-  let result = [];
+  const result = [];
 
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] > 0) break;
@@ -9,11 +9,11 @@ function threeSum(numbers) {
     let left = i + 1;
     let right = numbers.length - 1;
     while (left < right) {
-      let sum = numbers[i] + numbers[left] + numbers[right];
+      const sum = numbers[i] + numbers[left] + numbers[right];
 
-      if (sum < 0) {
+      if (sum > 0) {
         right--;
-      } else if (sum > 0) {
+      } else if (sum < 0) {
         left++;
       } else {
         result.push([numbers[i], numbers[left], numbers[right]]);
